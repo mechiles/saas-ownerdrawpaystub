1545ab16689f1cc0678c1c22be580b4ef6bbfe9f
Mark E. Chiles <krame18@gmail.com>
2023-02-27 16:09:15 -0600

    Re-arranged Dashboard for easier use on mobile

--- resources/views/themes/tailwind/dashboard/index.blade.php
+++ resources/views/themes/tailwind/dashboard/index.blade.php
@@ -26,25 +26,11 @@ $user = Auth::user()->id;
 	            <!-- <p class="text-base leading-loose text-gray-500">This is your application <a href="{{ route('wave.dashboard') }}" class="underline text-wave-500">dashboard</a>, you can customize this view inside of <code class="px-2 py-1 font-mono text-base font-medium text-gray-600 bg-gray-100 rounded-md">{{ theme_folder('/dashboard/index.blade.php') }}</code><br><br> (Themes are located inside the <code>resources/views/themes</code> folder)</p> -->
 
 				<p class="text-lg leading-5 text-gray-500 mt">
-	                    Below are your previous questions and answers:
-	                </p><p>&nbsp;</p><hr /><br />
-					<?php
-					$questions = DB::table('models')->where('user_id', $user)->orderBy('created_at', 'desc')->get();
-						if(count($questions) == 0)
-						echo '<p class="text-sm leading-5 text-gray-500 mt">You haven\'t asked any questions, yet.</p>';
-						
-						foreach ($questions as $question)
-						{
-							
-							echo '<p class="text-sm leading-5 text-gray-500 mt">Question: ' . $question->question . '<br /><br />';
-							echo "Answer: " . $question->answer . '</p><br /><hr /><br';
-							// var_dump($question->question, $question->answer);
-							
-						}
-						
-					?>
-				
-
+	                    <h3 class="text-lg font-medium leading-6 text-gray-700">
+						Let's get you some answers!
+	                </h3>
+	                <p class="text-sm leading-5 text-gray-500 mt">
+						Just click below to get started.</p>
 				<span class="inline-flex mt-5 rounded-md shadow-sm">
 	                <a href="{{ url('write') }}" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50">
 	                   Click Here For Answers
@@ -60,10 +46,10 @@ $user = Auth::user()->id;
 				</div>
 				<div class="relative flex-1">
 	                <h3 class="text-lg font-medium leading-6 text-gray-700">
-						Let's get you some answers!
+						Below are your previous questions and answers:
 	                </h3>
 	                <p class="text-sm leading-5 text-gray-500 mt">
-						Just ask your question below and we'll provide you an answer.
+						
 	                </p>
 				</div>
 
@@ -73,25 +59,31 @@ $user = Auth::user()->id;
 				<?php $openapi = env('OPEN_API_KEY'); ?>
 
 				<div class="container">
-
+				
+						<?php
+					$questions = DB::table('models')->where('user_id', $user)->orderBy('created_at', 'desc')->get();
+						if(count($questions) == 0)
+						echo '<p class="text-sm leading-5 text-gray-500 mt">You haven\'t asked any questions, yet.</p>';
+						
+						foreach ($questions as $question)
+						{
+							
+							echo '<p class="text-sm leading-5 text-gray-500 mt">Question: ' . $question->question . '<br /><br />';
+							echo "Answer: " . $question->answer . '</p><br /><hr /><br';
+							// var_dump($question->question, $question->answer);
+							
+						}
+						
+					?>
 					<div class="chatbot-container">
 						<div class="chatbot-header">
-						<h2 class="text-base leading-loose text-gray-500">What can we help you with?</h2>
+						<h3 class="text-base leading-loose text-gray-500">Need to start a new question?</h2>
 						</div>
 						<span class="inline-flex mt-5 rounded-md shadow-sm">
 	                <a href="{{ url('write') }}" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50">
 	                   Click Here To Get Answers
 	                </a>
 				</span>
-						<!-- <div class="chatbot-content">
-
-						<form class="chatbot-form" action="/write/generate" method="post">
-							<textarea id="chatbot-input" name="question" rows="4" cols="50" required></textarea><br />
-							<button class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50" type="submit">Get Answer</button>
-						</form>
-						</div>
-						<div class="chatbot-message-container">
-					</div> -->
 				</div>
 
 				<!-- <script>
