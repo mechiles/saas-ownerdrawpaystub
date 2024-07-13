22f0dd06f7ce17bd99fbb772cfa0ec6c124d9f77
Mark Chiles <mark@chilesco.com>
2024-07-12 22:22:49 -0500

    updated subscription trial days

--- wave/src/Http/Controllers/StripeController.php
+++ wave/src/Http/Controllers/StripeController.php
@@ -29,9 +29,9 @@ class StripeController extends Controller
                 ],
             ],
             'mode' => 'subscription',
-            'mode' => 'payment', // use for one-time payments
+            // 'mode' => 'payment', // use for one-time payments
             'subscription_data' => [
-                'trial_period_days' => 0,
+                'trial_period_days' => 1,
             ],
             'tax_id_collection' => [
                 'enabled' => env('CASHIER_STRIPE_CALCULATE_TAXES')
@@ -122,7 +122,7 @@ class StripeController extends Controller
             $counter += 1;
         }
 
-        $trial_days = 0; // set Stripe subscription trial days
+        $trial_days = 1; // set Stripe subscription trial days
         $trial_ends_at = null;
         // if trial days is not zero we will set trial_ends_at to ending date
         if (intval($trial_days) > 0) {
